6c9f07776550270689651dd26d2e035c882ae262
decentKG <decentgwavava@gmail.com>
2025-04-02 12:16:04 +0200

    new chapter

--- QRcode.py
+++ QRcode.py
@@ -1,14 +1,16 @@
-name = input("Enter your name: ").upper()
-surname = input("Enter yoour surname: ").upper()
-email = "kufahakuurayi@gmail.com"
-password = "kupakwashe"
-emails = input("Enter your email: ")
-correct = input("Enter your password: ")
+# name = input("Enter your name: ").upper()
+# surname = input("Enter yoour surname: ").upper()
+# email = "kufahakuurayi@gmail.com"
+# password = "kupakwashe"
+# emails = input("Enter your email: ")
+# correct = input("Enter your password: ")
 
 # while emails != email or correct != password :
 #     print("Please make sure your Email or Password is correct")
 #     emails = input("Enter your email: ")
 #     correct = input("Enter your password: ")
+
+print("Hello, World")
 
 
 
